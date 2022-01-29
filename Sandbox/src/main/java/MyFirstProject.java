56ee109c7b723f9e1dc40244089157f2386b3f58
karynochka <karinakkir@gmail.com>
2022-01-29 12:42:29 -0500

    расстояния между двумя точками

--- Sandbox/src/main/java/MyFirstProject.java
+++ Sandbox/src/main/java/MyFirstProject.java
@@ -1,6 +1,21 @@
 public class MyFirstProject {
 	
 	public static void main (String[] args) {
-		System.out.println("Hello, world!");
+
+		Point p1 = new Point();
+		p1.x = 2;
+		p1.y = 3;
+		Point p2 = new Point();
+		p2.x = 5;
+		p2.y = 7;
+		double distance = distance(p1,p2);
+		System.out.println ("Distance between points x1 = " + p1.x + " y1 = " + p1.y + " and x2 = "+ p2.x + " y2 = "+p2.y+ " = " + distance);
+	}
+
+	public static double distance (Point a, Point b){
+		double katet1 = b.x - a.x;
+		double katet2 = b.y - a.y;
+		double gipotenuza = Math.sqrt((katet1*katet1)+(katet2*katet2));
+		return gipotenuza;
 	}
 }
