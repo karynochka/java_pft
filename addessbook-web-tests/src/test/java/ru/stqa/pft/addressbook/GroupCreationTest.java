a9faf09cee7c6ee62ba506c760ee7abf251dad20
karynochka <karinakkir@gmail.com>
2022-02-04 14:24:17 -0500

    создания новой группы в адресной книге первая компиляция

--- addessbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ addessbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -1,40 +1,36 @@
-package com.example.GroupCreationTest;
+package ru.stqa.pft.addressbook;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class GroupCreationTest {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
-  private JavascriptExecutor js;
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    System.setProperty("webdriver.chrome.driver", "");
-    driver = new ChromeDriver();
+    driver = new FirefoxDriver();
     baseUrl = "https://www.google.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    js = (JavascriptExecutor) driver;
   }
 
   @Test
   public void testGroupCreation() throws Exception {
-    driver.get("http://localhost/addressbook/");
-    driver.findElement(By.xpath("//body")).click();
+    driver.get("http://localhost/addressbook/group.php");
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).clear();
     driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("//input[@value='Login']")).click();
-    driver.findElement(By.linkText("groups")).click();
+    driver.findElement(By.id("container")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
@@ -43,6 +39,7 @@ public class GroupCreationTest {
     driver.findElement(By.name("group_header")).sendKeys("test2");
     driver.findElement(By.name("group_footer")).clear();
     driver.findElement(By.name("group_footer")).sendKeys("test3");
+    driver.findElement(By.xpath("//form[@action='/addressbook/group.php']")).click();
     driver.findElement(By.name("submit")).click();
     driver.findElement(By.linkText("group page")).click();
     driver.findElement(By.linkText("Logout")).click();
