3b71f256439a0d1ac9e4faf0173df8fe473c9110
karynochka <karinakkir@gmail.com>
2022-02-06 16:06:59 -0500

    Обьект GroupData

--- addessbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ addessbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -1,38 +1,38 @@
 package ru.stqa.pft.addressbook;
 
 import java.util.concurrent.TimeUnit;
+
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GroupCreationTest {
-  private WebDriver wd;
+    private WebDriver wd;
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/group.php");
-      login();
-  }
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/group.php");
+        login("admin", "secret");
+    }
 
-    private void login() {
+    private void login(String username, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
     @Test
-  public void testGroupCreation() throws Exception {
+    public void testGroupCreation() throws Exception {
         gotoGroupPage();
         initGroupCreation();
-      fillGroupForm();
-      submitGroupCreation();
-      returnToGroupPage();
+        fillGroupForm(new GroupData("test2", "test1", "test3"));
+        submitGroupCreation();
+        returnToGroupPage();
         logoutGroupPage();
     }
 
@@ -41,25 +41,24 @@ public class GroupCreationTest {
     }
 
     private void returnToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
-  }
+        wd.findElement(By.linkText("group page")).click();
+    }
 
-  private void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
+    private void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
 
-  private void fillGroupForm() {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("test1");
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("test2");
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("test3");
-    wd.findElement(By.xpath("//form[@action='/addressbook/group.php']")).click();
-  }
+    private void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
 
-  private void initGroupCreation() {
+    private void initGroupCreation() {
         wd.findElement(By.name("new")).click();
     }
 
@@ -68,26 +67,26 @@ public class GroupCreationTest {
     }
 
     @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-      }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
+    public void tearDown() throws Exception {
+        wd.quit();
     }
-  }
 
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
-  }
 
-  }
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+}
