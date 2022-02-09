263bd32026a42a378c837b526353c4aa1dd666c3
karynochka <karinakkir@gmail.com>
2022-02-08 20:39:24 -0500

    task 4 - Test for Contacts

---
+++ addessbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String namel;
+    private final String company;
+    private final String address;
+    private final String phone;
+    private final String email;
+    private final String namef;
+
+    public ContactData(String namel, String company, String address, String phone, String email, String namef) {
+        this.namel = namel;
+        this.company = company;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.namef = namef;
+    }
+
+    public String getNamel() {
+        return namel;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNamef() {
+        return namef;
+    }
+}
